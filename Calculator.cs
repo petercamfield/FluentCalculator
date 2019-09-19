1c3f4439c50120f8c03c29bfbb7c73c14f333c3f
Peter Camfield <peter.camfield@clear.bank>
2019-09-19 09:07:36 +0100

    Empty solution for fluent calculator

---
+++ Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace FluentCalculator
+{
+    public class Calculator
+    {
+        
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FluentCalculator
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
